b9b3cd88224f06f385aa03adf72ea99341951903
Walt-20 <walt.c0des20@gmail.com>
2023-10-04 20:01:18 -0600

    changed schema table to hold ratio, bounding limits

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -22,11 +22,16 @@ class Graph extends Component<IProps, {}> {
     // Get element from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
+    // change the schema of the table
+    // allows for ratio with bound limits tracking
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float',
       timestamp: 'date',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float',
     };
 
     if (window.perspective && window.perspective.worker()) {
